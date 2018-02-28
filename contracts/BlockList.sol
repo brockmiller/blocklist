0c9a9beb6ca833147737102c25a6b28a5770b160
Brock Miller <brock@rolltape.com>
2018-02-28 10:04:54 -0800

    Add basic event when an item is posted

--- contracts/BlockList.sol
+++ contracts/BlockList.sol
@@ -8,11 +8,19 @@ contract BlockList {
   string description;
   uint256 price;
 
+  event LogSellItem(
+    address indexed _seller,
+    string _name,
+    uint256 _price
+  );
+
   function sellItem(string _name, string _description, uint256 _price) public {
     seller = msg.sender;
     name = _name;
     description = _description;
     price = _price;
+
+    LogSellItem(seller, name, price);
   }
 
   function getItem() public view returns (
--- src/js/app.js
+++ src/js/app.js
@@ -1,32 +1,131 @@
 App = {
-     web3Provider: null,
-     contracts: {},
+  web3Provider: null,
+  contracts: {},
+  account: 0x0,
 
-     init: function() {
-          /*
-           * Replace me...
-           */
+  init: function() {
+    return App.initWeb3();
+  },
 
-          return App.initWeb3();
-     },
+  initWeb3: function() {
+    // initialize web3
+    if(typeof web3 !== 'undefined') {
+      // use provider of the Web3 object injected by Metamask
+      App.web3Provider = web3.currentProvider;
+    } else {
+      // create a new provider which connects to my local ganache network
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+    }
+    web3 = new Web3(App.web3Provider);
 
-     initWeb3: function() {
-          /*
-           * Replace me...
-           */
+    App.displayAccountInfo();
 
-          return App.initContract();
-     },
+    return App.initContract();
+  },
 
-     initContract: function() {
-          /*
-           * Replace me...
-           */
-     },
+  displayAccountInfo: function() {
+    web3.eth.getCoinbase((err, account) => {
+      if(err === null) {
+        App.account = account;
+        // Inject the account number into the HTML
+        $('#account').text(account);
+
+        web3.eth.getBalance(account, (err, balance) => {
+          if(err === null) {
+            $('#accountBalance').text(web3.fromWei(balance, "ether") + " ETH");
+          }
+        });
+      }
+    });
+  },
+
+  initContract: function() {
+    $.getJSON('BlockList.json', (artifact) => {
+      // get the contract artifact file and use it to instantiate a truffle contract abstraction
+      App.contracts.BlockList = TruffleContract(artifact);
+      // set the provider for our contracts
+      App.contracts.BlockList.setProvider(App.web3Provider);
+
+      App.listenToEvents();
+
+      return App.reloadItems();
+    });
+  },
+
+  reloadItems: function() {
+    // refresh account information because the balance might have changed
+    App.displayAccountInfo();
+
+    // retrieve the item placeholder and clear it
+    $('#itemsRow').empty();
+
+    App.contracts.BlockList.deployed().then((instance) => {
+      return instance.getItem();
+    }).then((item) => {
+      if(item[0] == 0x0) {
+        // no item
+        return;
+      }
+
+      // retrieve the item template and fill it
+      var itemTemplate = $('#itemTemplate');
+      itemTemplate.find('.panel-title').text(item[1]);
+      itemTemplate.find('.item-description').text(item[2]);
+      itemTemplate.find('.item-price').text(web3.fromWei(item[3], "ether"));
+
+      var seller = item[0];
+      if (seller == App.account) {
+        seller = "You";
+      }
+      itemTemplate.find('.item-seller').text(seller);
+
+      // add this item
+      $('#itemsRow').append(itemTemplate.html());
+    }).catch((err) => {
+      console.error(err.message);
+    });
+  },
+
+  sellItem: function() {
+    // retrieve the detail of the item
+    var _item_name = $('#item_name').val();
+    var _description = $('#item_description').val();
+    var _price = web3.toWei(parseFloat($('#item_price').val() || 0), "ether");
+
+    if((_item_name.trim() == '') || (_price == 0)) {
+      // nothing to sell
+      return false;
+    }
+
+    App.contracts.BlockList.deployed().then((instance) => {
+      return instance.sellItem(_item_name, _description, _price, {
+        from: App.account,
+        gas: 500000
+      });
+    }).then((result) => {
+      // success
+    }).catch((err) => {
+      console.error(err);
+    });
+  },
+
+  // listen to events triggered by the contract
+  listenToEvents: function() {
+    App.contracts.BlockList.deployed().then((instance) => {
+      instance.LogSellItem({}, {}).watch((error, event) => {
+        if (!error) {
+          $("#events").append('<li class="list-group-item">' + event.args._name + ' is now for sale</li>');
+        } else {
+          console.error(error);
+        }
+        App.reloadItems();
+      });
+    });
+  },
 };
 
 $(function() {
-     $(window).load(function() {
-          App.init();
-     });
+  $(window).load(function() {
+    App.init();
+  });
 });
