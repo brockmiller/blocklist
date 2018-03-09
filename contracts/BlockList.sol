9699c2efa059e39d0ad753236f91f764442e10c1
Brock Miller <brock@rolltape.com>
2018-03-08 17:24:11 -0800

    Add contract method to buy and item + tests

--- contracts/BlockList.sol
+++ contracts/BlockList.sol
@@ -4,12 +4,19 @@ pragma solidity ^0.4.18;
 // of an existing item.
 contract BlockList {
   address seller;
+  address buyer;
   string name;
   string description;
   uint256 price;
 
   event LogSellItem(
     address indexed _seller,
+    string _name,
+    uint256 _price
+  );
+  event LogBuyItem(
+    address indexed _seller,
+    address indexed _buyer,
     string _name,
     uint256 _price
   );
@@ -25,10 +32,23 @@ contract BlockList {
 
   function getItem() public view returns (
     address _seller,
+    address _buyer,
     string _name,
     string _description,
     uint256 _price
   ) {
-    return (seller, name, description, price);
+    return (seller, buyer, name, description, price);
+  }
+
+  function buyItem() payable public {
+    require(seller != 0x0);
+    require(buyer == 0x0);
+    require(msg.sender != seller);
+    require(msg.value == price);
+
+    buyer = msg.sender;
+    seller.transfer(msg.value);
+
+    LogBuyItem(seller, buyer, name, price);
   }
 }
--- test/BlockListHappyPath.js
+++ test/BlockListHappyPath.js
@@ -3,9 +3,12 @@ var BlockList = artifacts.require('./BlockList.sol');
 contract('BlockList', function(accounts) {
   let blockListInstance;
   const seller = accounts[1];
+  const buyer = accounts[2];
   const itemName = 'uber hot sauce';
   const itemDescription = 'the hottest hot sauce';
   const itemPrice = 17;
+  let sellerBalanceBefore, sellerBalanceAfter;
+  let buyerBalanceBefore, buyerBalanceAfter;
 
   beforeEach(function() {
     return BlockList.deployed().then((instance) => {
@@ -16,21 +19,66 @@ contract('BlockList', function(accounts) {
   it('should initialize with an empty state', function() {
     return blockListInstance.getItem().then((data) => {
       assert.equal(data[0], 0x0, 'seller is empty');
-      assert.equal(data[1], '', 'name is empty');
-      assert.equal(data[2], '', 'description is empty');
-      assert.equal(data[3].toNumber(), 0, 'price is zero');
+      assert.equal(data[1], 0x0, 'buyer is empty');
+      assert.equal(data[2], '', 'name is empty');
+      assert.equal(data[3], '', 'description is empty');
+      assert.equal(data[4].toNumber(), 0, 'price is zero');
     });
   });
 
-  it('should sell an item', function() {
+  it('should sell an item', () => {
     return blockListInstance.
       sellItem(itemName, itemDescription, web3.toWei(itemPrice, 'ether'), { from: seller }).
       then(blockListInstance.getItem).
       then((data) => {
         assert.equal(data[0], seller);
-        assert.equal(data[1], itemName);
-        assert.equal(data[2], itemDescription);
-        assert.equal(data[3].toNumber(), web3.toWei(itemPrice, 'ether'));
+        assert.equal(data[1], 0x0);
+        assert.equal(data[2], itemName);
+        assert.equal(data[3], itemDescription);
+        assert.equal(data[4].toNumber(), web3.toWei(itemPrice, 'ether'));
       });
+  });
+
+  it('should buy an item', () => {
+    sellerBalanceBefore = web3.fromWei(web3.eth.getBalance(seller), 'ether').toNumber();
+    buyerBalanceBefore = web3.fromWei(web3.eth.getBalance(buyer), 'ether').toNumber();
+
+    return blockListInstance.buyItem({
+      from: buyer,
+      value: web3.toWei(itemPrice, 'ether')
+    }).then((receipt) => {
+      assert.equal(receipt.logs.length, 1);
+      assert.equal(receipt.logs[0].event, "LogBuyItem");
+      assert.equal(receipt.logs[0].args._seller, seller);
+      assert.equal(receipt.logs[0].args._buyer, buyer);
+      assert.equal(receipt.logs[0].args._name, itemName);
+      assert.equal(receipt.logs[0].args._price.toNumber(), web3.toWei(itemPrice, "ether"));
+
+      sellerBalanceAfter = web3.fromWei(web3.eth.getBalance(seller), "ether").toNumber();
+      buyerBalanceAfter = web3.fromWei(web3.eth.getBalance(buyer), "ether").toNumber();
+
+      assert(sellerBalanceAfter == sellerBalanceBefore + itemPrice);
+      assert(buyerBalanceAfter <= buyerBalanceBefore - itemPrice);
+
+      return blockListInstance.getItem();
+    }).then((data) => {
+      assert.equal(data[0], seller);
+      assert.equal(data[1], buyer);
+      assert.equal(data[2], itemName);
+      assert.equal(data[3], itemDescription);
+      assert.equal(data[4].toNumber(), web3.toWei(itemPrice, 'ether'));
+    });
+  });
+
+  it('should trigger an event when an item is sold', () => {
+    return blockListInstance.
+      sellItem(itemName, itemDescription, web3.toWei(itemPrice, "ether"), { from: seller }).
+      then((receipt) => {
+        assert.equal(receipt.logs.length, 1);
+        assert.equal(receipt.logs[0].event, "LogSellItem");
+        assert.equal(receipt.logs[0].args._seller, seller);
+        assert.equal(receipt.logs[0].args._name, itemName);
+        assert.equal(receipt.logs[0].args._price.toNumber(), web3.toWei(itemPrice, "ether"));
+    });
   });
 });
